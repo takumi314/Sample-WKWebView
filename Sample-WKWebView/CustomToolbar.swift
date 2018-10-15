12a6a5d37a9f0b41dea6be8308531ba1cba20614
takumi314 <isospin_symmetry@me.com>
2018-10-16 01:25:20 +0900

    [WIP] make custom UIToolBar class

---
+++ Sample-WKWebView/CustomToolbar.swift
@@ -0,0 +1,21 @@
+//
+//  CustomToolbar.swift
+//  Sample-WKWebView
+//
+//  Created by NishiokaKohei on 2018/10/16.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class CustomToolbar: UIToolbar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
